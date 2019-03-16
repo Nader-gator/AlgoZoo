2e0f5ca736d78550db11ab0b9831509a3ea4c893
Nader Arbabian <Nader.arb72@gmail.com>
2019-03-16 13:39:41 -0400

    add d3 canvas

--- zoo/static/scripts/bubblesort.js
+++ zoo/static/scripts/bubblesort.js
@@ -1,4 +1,7 @@
-document.addEventListener("DOMContentLoaded", ()=>{
-  let text = document.getElementById('text')
-  text.innerText = "fuck off"
+document.addEventListener("DOMContentLoaded", () => {
+  const canvas = d3.select(".bubble-sort").append("svg")
+    .attr('width', window.innerWidth * 0.8)
+    .attr("height", window.innerHeight * 0.3)
+
+  
 })
