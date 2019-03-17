71fe7b9d2e2e226a09960664b7534de5249fc0ec
Nader Arbabian <Nader.arb72@gmail.com>
2019-03-16 21:44:45 -0400

    add modified bubblesort

--- zoo/static/scripts/bubblesort.js
+++ zoo/static/scripts/bubblesort.js
@@ -1,7 +1,29 @@
-document.addEventListener("DOMContentLoaded", () => {
-  const canvas = d3.select(".bubble-sort").append("svg")
-    .attr('width', window.innerWidth * 0.8)
-    .attr("height", window.innerHeight * 0.3)
+document.addEventListener("DOMContentLoaded", runD3 )
+function runD3(){
+const canvas = d3.select(".bubble-sort").append("svg")
+  .attr('width', window.innerWidth * 0.8)
+  .attr("height", window.innerHeight * 0.3)
 
-  
-})
+
+
+function swap(array, i, j) {
+  var temp = array[i];
+  array[i] = array[j];
+  array[j] = temp;
+}
+function bubbleSort(array) {
+  var swapped = true;
+  var swaps = []
+  while (swapped) {
+    swapped = false;
+    for (var i = 0; i < array.length; i++) {
+      if (array[i] && array[i + 1] && array[i] > array[i + 1]) {
+        swap(array, i, i + 1);
+        swaps.push([i,i+1])
+        swapped = true;
+      }
+    }
+  } 
+  return array;
+}
+}
