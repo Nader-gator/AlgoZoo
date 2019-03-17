a1c8b0828e51b4c2c47c1239d3a8dc50eb7f9def
Nader Arbabian <Nader.arb72@gmail.com>
2019-03-17 00:03:59 -0400

    complete bubblesort

--- zoo/static/scripts/bubblesort.js
+++ zoo/static/scripts/bubblesort.js
@@ -3,13 +3,13 @@ function runD3(){
   var width = 800,
       height = 500;
 
-  const canvas = d3.select(".bubble-sort").append("svg")
+  var canvas = d3.select(".bubble-sort").append("svg")
     .attr('width', width)
     .attr("height", height)
     .append('g')
     .attr('transform',`translate(100,300)`)
 
-  var n = 20,
+  var n = 15,
       array = d3.shuffle(d3.range(n)),
       swaps = bubbleSort(array.slice()).reverse(),
       height = 500,
@@ -24,12 +24,11 @@ function runD3(){
         .domain([0,n-1])
         .range([50,200])
 
-  const lines = canvas.append("g")
+  var lines = canvas.append("g")
     .attr("class", "line")
     .selectAll('line')
     .data(array)
-    .enter()
-    .append('line')
+    .enter().append('line')
     .attr('y2', heightT)
     .attr('transform', transform)
     .attr('stroke', color)
@@ -45,6 +44,22 @@ function runD3(){
     return `-${heightScale(d)}`
   }
 
+  var transition = d3.transition()
+    .duration(250)
+    .on("start", function start(){
+      var swap = swaps.pop(),
+        i = swap[0],
+        j = swap[1],
+        lineI = lines._groups[0][i],
+        lineJ = lines._groups[0][j];
+
+        lines._groups[0][i] = lineJ
+        lines._groups[0][j] = lineI
+        transition.each(function ()
+          {lines.transition().attr('transform', transform)})
+        debugger
+        if (swaps.length) {transition = transition.transition().on('start',start)}
+    })
 
 
   function swap(array, i, j) {
@@ -57,8 +72,8 @@ function runD3(){
     let swaps = []
     while (swapped) {
       swapped = false;
-      for (var i = 0; i < array.length; i++) {
-        if (array[i] && array[i + 1] && array[i] > array[i + 1]) {
+      for (var i = 0; i < array.length -1; i++) {
+        if (array[i] > array[i + 1]) {
           swap(array, i, i + 1);
           swaps.push([i,i+1]);
           swapped = true;
