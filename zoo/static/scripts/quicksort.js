c5f919f65f5b4ba0e2dc714168c88a20d7b6574c
Nader Arbabian <Nader.arb72@gmail.com>
2019-03-17 22:00:19 -0400

    finish quicksort

--- zoo/static/scripts/quicksort.js
+++ zoo/static/scripts/quicksort.js
@@ -4,85 +4,92 @@ function runD3() {
   var width = 800,
     height = 500;
 
-  // var canvas = d3.select(".quick-sort").append("svg")
-  //   .attr('width', width)
-  //   .attr("height", height)
-  //   .append('g')
-  //   .attr('transform', `translate(100,300)`)
+  var canvas = d3.select(".quick-sort").append("svg")
+    .attr('width', width)
+    .attr("height", height)
+    .append('g')
+    .attr('transform', `translate(100,300)`)
 
-  // var n = 15,
-  //   array = d3.shuffle(d3.range(n)),
-  //   swaps = quickSort(array.slice()).reverse(),
-  //   comparisons = quickSort(array.slice()).reverse(),
-  //   height = 500,
-  //   width = 800,
-  //   xScale = d3.scaleLinear()
-  //   .domain([0, n - 1])
-  //   .range([0, 600]),
-  //   rainbow = d3.scaleLinear()
-  //   .domain([0, n / 2, n - 1])
-  //   .range(['red', 'green', 'blue']),
-  //   heightScale = d3.scaleLinear()
-  //   .domain([0, n - 1])
-  //   .range([50, 250])
+  var n = 25,
+    array = d3.shuffle(d3.range(n)),
+    moves = quickSort(array.slice()).reverse(),
+    height = 500,
+    width = 800,
+    xScale = d3.scaleLinear()
+    .domain([0, n - 1])
+    .range([0, 600]),
+    rainbow = d3.scaleLinear()
+    .domain([0, n / 2, n - 1])
+    .range(['red', 'green', 'blue']),
+    heightScale = d3.scaleLinear()
+    .domain([0, n - 1])
+    .range([50, 250])
 
-  // var lines = canvas.append("g")
-  //   .attr("class", "line")
-  //   .selectAll('line')
-  //   .data(array)
-  //   .enter().append('line')
-  //   .attr('y2', heightT)
-  //   .attr('transform', transform)
-  //   .attr('stroke', color)
+  var lines = canvas.append("g")
+    .attr("class", "line")
+    .selectAll('line')
+    .data(array)
+    .enter().append('line')
+    .attr('y2', heightT)
+    .attr('transform', transform)
+    // .attr('stroke', color)
 
-  // var underline = canvas.append("g")
-  //   .data(swaps)
-  //   .attr("class", "underline")
-  //   .append("line")
-  //   .attr("x2", xScale(1) + 10)
-  //   .attr("x1", xScale(0) - 10)
-  //   .attr("transform", "translate(0,10)")
-  //   .attr("stroke", "red")
+  function transform(d, i) {
+    return `translate(${xScale(i)})`
+  }
 
+  function color(d) {
+    return rainbow(d)
+  }
 
-  // function transform(d, i) {
-  //   return `translate(${xScale(i)})`
-  // }
+  function heightT(d) {
+    return heightScale(d) * -1
+  }
 
-  // function color(d) {
-  //   return rainbow(d)
-  // }
+  var transition = d3.transition().delay(200)
+    .duration(650)
+    .on("start", function start() {
+      var move = moves.pop()
+      if (move.type === "swap"){
+        swapBars(move)
+      }else if (move.type === "split"){
+        grayOut(move)
+      }
+      
+      if (moves.length) {
+        transition = transition.transition().on('start', start)
+      } else {
+        colorizeAll()
+      }
+    })
 
-  // function heightT(d) {
-  //   return heightScale(d) * -1
-  // }
+  function swapBars(move){
+    var i = move.first,
+        j = move.second,
+        lineI = lines._groups[0][i],
+        lineJ = lines._groups[0][j];
 
-  // function underlineT(swap) {
-  //   return `translate(${swap[0]},${swap[1]})`
-  // }
+    lines._groups[0][i] = lineJ
+    lines._groups[0][j] = lineI
+    transition.each(function () {
+      lines.transition().attr('transform', transform)
+    })
+  }
 
-  // var transition = d3.transition().delay(500)
-  //   .duration(750)
-  //   .on("start", function start() {
-  //     var swap = swaps.pop(),
-  //       comparison = comparisons.pop()
-  //     i = swap[0],
-  //       j = swap[1],
-  //       lineI = lines._groups[0][i],
-  //       lineJ = lines._groups[0][j];
+  function grayOut(move){
+    lines.attr('class',function(d,i){
+      debugger
+      if (i < move.left || move.right < i){
+        return 'greyed'
+      } else if (i === move.pivotPoint){
+        return "pivot"
+      }
+    })
+  }
 
-  //     lines._groups[0][i] = lineJ
-  //     lines._groups[0][j] = lineI
-  //     transition.each(function () {
-  //       lines.transition().attr('transform', transform)
-  //       underline.transition().attr('x1', xScale(comparison[0]) - 10).attr('x2', xScale(comparison[1]) + 10)
-  //     })
-  //     if (swaps.length) {
-  //       transition = transition.transition().on('start', start)
-  //     }
-  //   })
+  function colorizeAll(){
 
-
+  }
 
 function quickSort(array){
   moves = []
@@ -125,38 +132,4 @@ function quickSort(array){
   split(0,array.length)
   return moves
 }
-
-// function quickSort(array) {
-//   var actions = [];
-
-//   function partition(left, right, pivot) {
-//     var v = array[pivot];
-//     swap(pivot, --right);
-//     for (var i = left; i < right; ++i)
-//       if (array[i] <= v) swap(i, left++);
-//     swap(left, right);
-//     return left;
-//   }
-
-
-//   function recurse(left, right) {
-//     if (left < right - 1) {
-//       var pivot = (left + right) >> 1;
-//       actions.push({
-//         type: "partition",
-//         "left": left,
-//         "pivot": pivot,
-//         "right": right
-//       });
-//       pivot = partition(left, right, pivot);
-//       recurse(left, pivot);
-//       recurse(pivot + 1, right);
-//     }
-//   }
-
-//   recurse(0, array.length);
-//   return actions;
-// }
-
-quickSort([6,4,8,3])
 }
