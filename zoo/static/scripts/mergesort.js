c035dc0696f91141c22bd2fe84581d8c76ed5807
Nader Arbabian <Nader.arb72@gmail.com>
2019-03-18 20:14:38 -0400

    add layers

--- zoo/static/scripts/mergesort.js
+++ zoo/static/scripts/mergesort.js
@@ -2,20 +2,32 @@ document.addEventListener("DOMContentLoaded", runD3)
 
 function runD3() {
   var width = 800,
-    height = 500;
+    height = 500 / 3;
 
-  var canvas = d3.select(".merge-sort").append("svg")
+  var layer0 = d3.select(".merge-sort").append("svg")
     .attr('width', width)
     .attr("height", height)
     .append('g')
-    .attr('transform', `translate(100,300)`)
+    .attr('transform', `translate(100,100)`)
+  var layer1 = d3.select(".merge-sort").append("svg")
+    .attr('width', width)
+    .attr("height", height)
+    .append('g')
+    .attr('transform', `translate(100,100)`)
+  var layer2 = d3.select(".merge-sort").append("svg")
+    .attr('width', width)
+    .attr("height", height)
+    .append('g')
+    .attr('transform', `translate(100,100)`)
+  var layer3 = d3.select(".merge-sort").append("svg")
+    .attr('width', width)
+    .attr("height", height)
+    .append('g')
+    .attr('transform', `translate(100,100)`)
 
-  var n = 40,
+  var n = 8,
     array = d3.shuffle(d3.range(n)),
-    // swaps = bubbleSort(array.slice()).swaps.reverse(),
-    // comparisons = bubbleSort(array.slice()).comparisons.reverse(),
-    height = 500,
-    width = 800,
+    moves = mergeSort(array.slice()),
     xScale = d3.scaleLinear()
     .domain([0, n - 1])
     .range([0, 600]),
@@ -24,17 +36,19 @@ function runD3() {
     .range(['red', 'green', 'blue']),
     heightScale = d3.scaleLinear()
     .domain([0, n - 1])
-    .range([50, 250])
+    .range([10, 50 ])
 
 
-  var lines = canvas.append("g")
+  var lines = layer0.append("g")
     .attr("class", "line")
-    .selectAll('line')
+    .selectAll('rect')
     .data(array)
-    .enter().append('line')
-    .attr('y2', heightT)
+    .enter().append('rect')
+    .attr('width', heightT)
+    .attr('height', heightT)
     .attr('transform', transform)
     .attr('stroke', color)
+    .attr('fill', color)
 
   function transform(d, i) {
     return `translate(${xScale(i)})`
@@ -45,11 +59,12 @@ function runD3() {
   }
 
   function heightT(d) {
-    return heightScale(d) * -1
+    return heightScale(d)
   }
 
 
   function mergeSort(array){
+    var moves = []
     function merge(left, right) {
       result = []
       while (left.length && right.length){
@@ -64,16 +79,19 @@ function runD3() {
       return result.concat(left).concat(right)
     }
 
-    function split(array){
+    function split(array ,layer = 0){
       if (array.length < 2) return array;
       let middlePoint = array.length >> 1,
       left = array.slice(0,middlePoint),
       right = array.slice(middlePoint)
-
-      return merge(split(left),split(right))
+      moves.push({
+        type: "split",
+        middlePoint: middlePoint,
+        layer: layer
+      })
+      return merge(split(left,layer+1),split(right,layer+1))
     }
-
-    return split(array)
+   split(array)
+   return moves
   }
-  debugger
 }
